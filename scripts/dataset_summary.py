23172b12eea197bfe0718a9baaadc3fe9307cde2
MarcoFavorito <marco.favorito@gmail.com>
2020-02-20 22:19:01 +0100

    refine dataset summary.

--- scripts/dataset_summary.py
+++ scripts/dataset_summary.py
@@ -13,6 +13,21 @@ args = parser.parse_args()
 def _score_book_list(book_ids, score):
     return sum(map(lambda book_id: score[book_id], book_ids))
 
+
+def print_stats(data, label):
+    print("Avg {}: {}".format(label, np.mean(data)))
+    print("Std {}: {}".format(label, np.std(data)))
+    print("Max {}: {}".format(label, np.max(data)))
+    print("Min {}: {}".format(label, np.min(data)))
+    print("00th {}: {}".format(label, np.percentile(data, 0)  ))
+    print("25th {}: {}".format(label, np.percentile(data, 25) ))
+    print("50th {}: {}".format(label, np.percentile(data, 50) ))
+    print("75th {}: {}".format(label, np.percentile(data, 75) ))
+    print("100th {}: {}".format(label, np.percentile(data, 100)))
+    print("-"*50)
+
+
+
 if __name__ == '__main__':
     input_ = Input.read(args.in_file)  # type: Input
 
@@ -20,18 +35,11 @@ if __name__ == '__main__':
     print("# Book: {}".format(input_.nb_books))
     print("# Days: {}".format(input_.nb_days))
 
-    print("Avg Books per Library: {}".format(np.mean([len(l.books) for l in input_.libraries])))
-    print("Std Books per Library: {}".format(np.std([len(l.books) for l in input_.libraries])))
 
-    print("Avg Score per Library: {}".format(np.mean([_score_book_list(l.books, input_.scores) for l in input_.libraries])))
-    print("Std Score per Library: {}".format(np.std([_score_book_list(l.books, input_.scores) for l in input_.libraries])))
+    print_stats(input_.scores, "Book score")
+    print_stats([len(l.books) for l in input_.libraries], "Books per Library")
+    print_stats([_score_book_list(l.books, input_.scores) for l in input_.libraries], "Score per Library")
+    print_stats(list(map(lambda l: l.ship_book_rate, input_.libraries)), "Shipping rate")
+    print_stats(list(map(lambda l: l.nb_signup_days, input_.libraries)), "signup day period")
 
-    print("Avg Shipping rate: {}".format(np.mean(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
-    print("Std Shipping rate: {}".format(np.std(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
-
-    print("Avg signup day period: {}".format(np.mean(list(map(lambda l: l.nb_signup_days, input_.libraries)))))
-    print("Std signup day period: {}".format(np.std(list(map(lambda l: l.nb_signup_days, input_.libraries)))))
-
-    print("Avg Book score: {}".format(np.mean(input_.scores)))
-    print("Std Book score: {}".format(np.std(input_.scores)))
 
