151f501636316fd7dbeb37b0a8d9af16b35d9b03
MarcoFavorito <marco.favorito@gmail.com>
2020-02-20 21:27:57 +0100

    add sst and highest shipping rate

---
+++ hashcode20/sol/sst_and_highest_shipping_rate.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import random
+
+from hashcode20.helpers import Input, Output
+
+
+def main(i: Input) -> Output:
+    """naive: take order as the order of appearance"""
+    libraries_sorted = sorted(i.libraries, key=lambda library: (library.nb_signup_days, -library.ship_book_rate))
+    library_order = list(map(lambda l: l.library_id, libraries_sorted))
+    book_order_per_library = [list(l.books) for l in libraries_sorted]
+    return Output(library_order, book_order_per_library)
+
