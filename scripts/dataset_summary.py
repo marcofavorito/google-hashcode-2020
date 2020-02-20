38499ca525d6e780c4ebd6704f19189581ab21b4
MarcoFavorito <marco.favorito@gmail.com>
2020-02-20 22:05:56 +0100

    add avg score library.

--- scripts/dataset_summary.py
+++ scripts/dataset_summary.py
@@ -10,12 +10,21 @@ parser.add_argument("--in",  dest="in_file",  type=str, default=None, help="prov
 args = parser.parse_args()
 
 
+def _score_book_list(book_ids, score):
+    return sum(map(lambda book_id: score[book_id], book_ids))
+
 if __name__ == '__main__':
     input_ = Input.read(args.in_file)  # type: Input
 
     print("# Libraries: {}".format(len(input_.libraries)))
     print("# Book: {}".format(input_.nb_books))
     print("# Days: {}".format(input_.nb_days))
+
+    print("Avg Books per Library: {}".format(np.mean([len(l.books) for l in input_.libraries])))
+    print("Std Books per Library: {}".format(np.std([len(l.books) for l in input_.libraries])))
+
+    print("Avg Score per Library: {}".format(np.mean([_score_book_list(l.books, input_.scores) for l in input_.libraries])))
+    print("Std Score per Library: {}".format(np.mean([_score_book_list(l.books, input_.scores) for l in input_.libraries])))
 
     print("Avg Shipping rate: {}".format(np.mean(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
     print("Std Shipping rate: {}".format(np.std(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
