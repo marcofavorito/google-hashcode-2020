3c117ab632e632faac2645abd9313fc381f07be8
MarcoFavorito <marco.favorito@gmail.com>
2020-02-20 21:56:50 +0100

    improve strings in dataset summary

--- scripts/dataset_summary.py
+++ scripts/dataset_summary.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     print("# Libraries: {}".format(len(input_.libraries)))
     print("# Book: {}".format(len(input_.libraries)))
 
-    print("Avg Shipping rate time: {}".format(np.mean(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
-    print("Std Shipping rate time: {}".format(np.std(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
+    print("Avg Shipping rate: {}".format(np.mean(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
+    print("Std Shipping rate: {}".format(np.std(list(map(lambda l: l.ship_book_rate, input_.libraries)))))
 
-    print("Avg signup day: {}".format(np.mean(list(map(lambda l: l.nb_signup_days, input_.libraries)))))
-    print("Std signup day: {}".format(np.std(list(map(lambda l: l.nb_signup_days, input_.libraries)))))
+    print("Avg signup day period: {}".format(np.mean(list(map(lambda l: l.nb_signup_days, input_.libraries)))))
+    print("Std signup day period: {}".format(np.std(list(map(lambda l: l.nb_signup_days, input_.libraries)))))
 
     print("Avg Rate time: {}".format(np.mean(input_.scores)))
     print("Std Rate time: {}".format(np.std(input_.scores)))
